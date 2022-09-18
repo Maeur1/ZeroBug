2cd3454e5c08320ad2d950b843463f32b04bb260
Mayur Panchal <panchal.mayur2@gmail.com>
2022-09-18 23:08:25 +1000

    Add service

--- Raspberry Pi Software/setup.sh
+++ Raspberry Pi Software/setup.sh
@@ -1,2 +1,6 @@
-sudo apt-get install git curl libsdl2-mixer-2.0-0 libsdl2-image-2.0-0 libsdl2-2.0-0 cmake libjpeg9-dev
-
+sudo apt install pip git curl libsdl2-mixer-2.0-0 libsdl2-image-2.0-0 libsdl2-2.0-0 cmake libjpeg9-dev
+pip install -r requirements.txt
+sudo cp app.service /etc/systemd/system/app.service
+sudo systemctl daemon-reload
+sudo systemctl enable app.service
+sudo systemctl start app.service
